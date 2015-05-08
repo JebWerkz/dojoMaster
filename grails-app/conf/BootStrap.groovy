c75d7b76bc94fca9b2c5c213c2cb299a95044b50
Jeremiah Evans <exacerbating.the.situation@gmail.com>
2015-05-08 14:42:22 -0400

    Updates to configuration, including dev database

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,7 @@
 import com.jebwerkz.dojoMaster.art.Art
 import com.jebwerkz.dojoMaster.team.Team
 import com.jebwerkz.dojoMaster.rank.Rank
+import com.jebwerkz.dojoMaster.rank.RankTitle
 
 class BootStrap {
 
@@ -16,23 +17,48 @@ class BootStrap {
             new Team(name:"Blue Team", color:"Blue", description: "Building Leaders of Unmatched Excellence").save(failOnError: true)
         }
 
-        if(!Rank.count()) {
-            new Rank(art:1, belt:"White, Solid", uniform: "White", chevrons:"N/A", description: "Beginner", name: "White Belt", nameKr: "White Belt", team: 1, title:"None").save(failOnError: true)
-            new Rank(art:1, belt:"Yellow, Solid", uniform: "White", chevrons:"N/A", description: "Level 1", name: "Yellow Belt", nameKr: "Yellow Belt", team: 1, title:"None").save(failOnError: true)
-            new Rank(art:1, belt:"Orange, Solid", uniform: "White", chevrons:"N/A", description: "Level 1", name: "Orange Belt", nameKr: "Orange Belt", team: 1, title:"None").save(failOnError: true)
-            new Rank(art:1, belt:"Green, Solid", uniform: "White", chevrons:"N/A", description: "Level 1", name: "Green Belt", nameKr: "Green Belt", team: 1, title:"None").save(failOnError: true)
-            new Rank(art:1, belt:"Blue, Solid", uniform: "White", chevrons:"N/A", description: "Level 1", name: "Blue Belt", nameKr: "Blue Belt", team: 1, title:"None").save(failOnError: true)
-            new Rank(art:1, belt:"Brown, Solid", uniform: "White Top, Black Pants", chevrons:"N/A", description: "Level 2", name: "Brown Belt", nameKr: "Brown Belt", team: 1, title:"None").save(failOnError: true)
-            new Rank(art:1, belt:"Purple, Solid", uniform: "White Top, Black Pants", chevrons:"N/A", description: "Level 2", name: "Purple Belt", nameKr: "Purple Belt", team: 1, title:"None").save(failOnError: true)
-            new Rank(art:1, belt:"Red, Solid", uniform: "White Top, Black Pants", chevrons:"N/A", description: "Level 2", name: "Red Belt", nameKr: "Red Belt", team: 1, title:"None").save(failOnError: true)
-            new Rank(art:1, belt:"Black, Solid", uniform: "White Top with Black Collar, Black Pants", chevrons:"N/A", description: "Level 2", name: "Recommend Black Belt", nameKr: "Recommend Black  Belt", team: 1, title:"Mr/Ms").save(failOnError: true)
-            new Rank(art:1, belt:"Black, Gold Embroidery, 1 Stripes", uniform: "White Top with Black Collar, Black Pants", chevrons:"N/A", description: "Black", name: "1st Degree Black Belt", nameKr: "1st Dan", team: 1, title:"Mr/Ms").save(failOnError: true)
-            new Rank(art:1, belt:"Black, Gold Embroidery, 2 Stripes", uniform: "White Top with Black Collar, Black Pants", chevrons:"N/A", description: "Black", name: "2nd Degree Black Belt", nameKr: "2nd Dan", team: 1, title:"Mr/Ms").save(failOnError: true)
-            new Rank(art:1, belt:"Black, Gold Embroidery, 3 Stripes", uniform: "Black", chevrons:"N/A", description: "Black", name: "3rd Degree Black Belt", nameKr: "3rd Dan", team: 1, title:"Junior Master").save(failOnError: true)
-            new Rank(art:1, belt:"Black, Gold Embroidery, 4 Stripes", uniform: "Black", chevrons:"N/A", description: "Black", name: "4th Degree Black Belt", nameKr: "4th Dan", team: 1, title:"Junior Master").save(failOnError: true)
-            new Rank(art:1, belt:"Black, Gold Embroidery, 5 Stripes", uniform: "Black", chevrons:"N/A", description: "Black", name: "5th Degree Black Belt", nameKr: "5th Dan", team: 1, title:"Master").save(failOnError: true)
-            new Rank(art:1, belt:"Black, Gold Embroidery, 6 Stripes", uniform: "Black", chevrons:"N/A", description: "Black", name: "6th Degree Black Belt", nameKr: "6th Dan", team: 1, title:"Master").save(failOnError: true)
+        if(!RankTitle.count()) {
+            new RankTitle("None").save(failOnError: true)
+            new RankTitle("Mr./Ms.").save(failOnError: true)
+            new RankTitle("Junior Master").save(failOnError: true)
+            new RankTitle("Master").save(failOnError: true)
         }
+
+        if(!Rank.count()) {
+            //Junior Taekwondo Ranks
+
+
+            //Adult Taekwondo Ranks
+            new Rank(art:1, belt:"White, Solid", uniform: "White", chevrons:"N/A", description: "Beginner", name: "White Belt", nameKr: "White Belt", team: 1, title:1).save(failOnError: true)
+            new Rank(art:1, belt:"Yellow, Solid", uniform: "White", chevrons:"N/A", description: "Level 1", name: "Yellow Belt", nameKr: "Yellow Belt", team: 1, title:1).save(failOnError: true)
+            new Rank(art:1, belt:"Orange, Solid", uniform: "White", chevrons:"N/A", description: "Level 1", name: "Orange Belt", nameKr: "Orange Belt", team: 1, title:1).save(failOnError: true)
+            new Rank(art:1, belt:"Green, Solid", uniform: "White", chevrons:"N/A", description: "Level 1", name: "Green Belt", nameKr: "Green Belt", team: 1, title:1).save(failOnError: true)
+            new Rank(art:1, belt:"Blue, Solid", uniform: "White", chevrons:"N/A", description: "Level 1", name: "Blue Belt", nameKr: "Blue Belt", team: 1, title:1).save(failOnError: true)
+            new Rank(art:1, belt:"Brown, Solid", uniform: "White Top, Black Pants", chevrons:"N/A", description: "Level 2", name: "Brown Belt", nameKr: "Brown Belt", team: 1, title:1).save(failOnError: true)
+            new Rank(art:1, belt:"Purple, Solid", uniform: "White Top, Black Pants", chevrons:"N/A", description: "Level 2", name: "Purple Belt", nameKr: "Purple Belt", team: 1, title:1).save(failOnError: true)
+            new Rank(art:1, belt:"Red, Solid", uniform: "White Top, Black Pants", chevrons:"N/A", description: "Level 2", name: "Red Belt", nameKr: "Red Belt", team: 1, title:1).save(failOnError: true)
+            new Rank(art:1, belt:"Black, Solid", uniform: "White Top with Black Collar, Black Pants", chevrons:"N/A", description: "Level 2", name: "Recommend Black Belt", nameKr: "Recommend Black  Belt", team: 1, title:2).save(failOnError: true)
+            new Rank(art:1, belt:"Black, Gold Embroidery, 1 Stripes", uniform: "White Top with Black Collar, Black Pants", chevrons:"N/A", description: "Black", name: "1st Degree Black Belt", nameKr: "1st Dan", team: 1, title:2).save(failOnError: true)
+            new Rank(art:1, belt:"Black, Gold Embroidery, 2 Stripes", uniform: "White Top with Black Collar, Black Pants", chevrons:"N/A", description: "Black", name: "2nd Degree Black Belt", nameKr: "2nd Dan", team: 1, title:2).save(failOnError: true)
+            new Rank(art:1, belt:"Black, Gold Embroidery, 3 Stripes", uniform: "Black", chevrons:"N/A", description: "Black", name: "3rd Degree Black Belt", nameKr: "3rd Dan", team: 1, title:3).save(failOnError: true)
+            new Rank(art:1, belt:"Black, Gold Embroidery, 4 Stripes", uniform: "Black", chevrons:"N/A", description: "Black", name: "4th Degree Black Belt", nameKr: "4th Dan", team: 1, title:3).save(failOnError: true)
+            new Rank(art:1, belt:"Black, Gold Embroidery, 5 Stripes", uniform: "Black", chevrons:"N/A", description: "Black", name: "5th Degree Black Belt", nameKr: "5th Dan", team: 1, title:4).save(failOnError: true)
+            new Rank(art:1, belt:"Black, Gold Embroidery, 6 Stripes", uniform: "Black", chevrons:"N/A", description: "Black", name: "6th Degree Black Belt", nameKr: "6th Dan", team: 1, title:4).save(failOnError: true)
+        
+            //Hapkido Ranks
+            new Rank(art:2, belt:"White, Solid", uniform: "White", chevrons:"N/A", description: "Beginner", name: "White Belt", nameKr: "White Belt", team: 1, title:1).save(failOnError: true)
+            new Rank(art:2, belt:"Yellow, Solid", uniform: "White", chevrons:"N/A", description: "Level 1", name: "Yellow Belt", nameKr: "Yellow Belt", team: 1, title:1).save(failOnError: true)
+
+            //Junior Mukido Ranks
+            
+
+            //Adult Mukido Ranks
+        }
+
+
+
+
+
     }
     def destroy = {
     }
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -22,12 +22,14 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/martial_arts_dev?useUnicode=yes&characterEncoding=UTF-8"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
 //            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mysql://localhost/martial_arts?useUnicode=yes&characterEncoding=UTF-8"
         }
     }
     production {
